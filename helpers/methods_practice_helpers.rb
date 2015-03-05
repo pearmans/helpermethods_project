068510c0cbe29dc295135bdf60f90525a7b1a592
Stuart Pearman <stuart@element84.com>
2015-03-05 09:24:49 -0500

    adjusted formatting in helper file

--- helpers/methods_practice_helpers.rb
+++ helpers/methods_practice_helpers.rb
@@ -4,7 +4,7 @@ module MethodsPracticeHelpers
     # Generate an image tag with the given source and the classes
     # "img-responsive" and "img-thumbnail". The image should also have an alt
     # attribute with the contents of alternate_text.
-    %(<img src="#{source}" alt="#{alternate_text}" class="img-responsive img-thumbnail" />)
+    %( <img src="#{source}" alt="#{alternate_text}" class="img-responsive img-thumbnail" /> )
   end
 
   def bootstrap_alert(message, type='info')
@@ -12,12 +12,14 @@ module MethodsPracticeHelpers
     # to remember all HTML needed to do it right). It should show the message in
     # the "message" variable and should have a class appropriate for the "type"
     # that is passed in, defaulting to 'info' if no type is passed in.
-    %(<div class="alert alert-#{type} alert-dismissible" role="alert">
+    %(
+      <div class="alert alert-#{type} alert-dismissible" role="alert">
         <button type="button" class="close" data-dismiss="alert" aria-label="Close">
           <span aria-hidden="true">&times;</span>
         </button>
         #{message}
-      </div>)
+      </div>
+    )
   end
 
   def current_date_and_time
